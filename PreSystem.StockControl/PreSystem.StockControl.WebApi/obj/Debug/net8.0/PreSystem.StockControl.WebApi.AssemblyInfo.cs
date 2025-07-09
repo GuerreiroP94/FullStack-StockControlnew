1d3e35e91fcaa3e3e4be17e4a922d3bdb79c95d8
GabrielP94 <gabriellucker@gmail.com>
2025-07-09 14:59:45 -0300

    Fix: Simplificar configuração - resolver conflitos de SDK e packages

--- PreSystem.StockControl/PreSystem.StockControl.WebApi/obj/Debug/net8.0/PreSystem.StockControl.WebApi.AssemblyInfo.cs
+++ PreSystem.StockControl/PreSystem.StockControl.WebApi/obj/Debug/net8.0/PreSystem.StockControl.WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PreSystem.StockControl.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff3b3fff457bbd6ee01d30cce064833e969220a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+915ff3c45a253bdf055d8e6f8cb42305ea3b3b02")]
 [assembly: System.Reflection.AssemblyProductAttribute("PreSystem.StockControl.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PreSystem.StockControl.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
